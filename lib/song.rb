7a629856550dec01055ae9a1d5c6894050deb366
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 04:10:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
-require 'pry'
-
-class Song
-  attr_accessor :name, :artist
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
+# require 'pry'
+# 
+# class Song
+#   attr_accessor :name, :artist
+# 
+#   @@all = []
+# 
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
 
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1]
@@ -28,32 +28,32 @@ class Song
 
 end
 
-# class Song
-#   attr_accessor :name, :artist
-#   @@all = []
-#
-#   def initialize(name)
-#     @name = name
-#     save
-#   end
-#
-#   def save
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def self.new_by_filename(filename)
-#     artist, song = filename.split(" - ")
-#     new_song = self.new(song)
-#     new_song.artist_name = artist
-#     new_song
-#   end
-#
-#   def artist_name=(name)
-#     self.artist = Artist.find_or_create_by_name(name)
-#     artist.add_song(self)
-#   end
-# end
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+end
