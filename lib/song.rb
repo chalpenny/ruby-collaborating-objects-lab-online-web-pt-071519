39a1e5a11ffb12feef98f6111ad2260c513164fc
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 04:11:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,19 +14,19 @@
 #     @@all
 #   end
 
-  def self.new_by_filename(filename)
-    song = filename.split(" - ")[1]
-    artist = filename.split(" - ")[0]
-    new_song = Song.new(song)
-    new_song.artist = artist
-    new_song
-  end
-
-  def artist_name= (artist)
-    @@all << self.artist = Artist.find_or_create_by_name(artist)
-  end
-
-end
+#   def self.new_by_filename(filename)
+#     song = filename.split(" - ")[1]
+#     artist = filename.split(" - ")[0]
+#     new_song = Song.new(song)
+#     new_song.artist = artist
+#     new_song
+#   end
+# 
+#   def artist_name= (artist)
+#     @@all << self.artist = Artist.find_or_create_by_name(artist)
+#   end
+# 
+# end
 
 class Song
   attr_accessor :name, :artist
