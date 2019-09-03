6fd24e4624d45e2e43ef85471d89c0d67f443b49
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 04:14:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,59 +1,26 @@
 require 'pry'
-#
-# class Artist
-#   attr_accessor :name, :songs
-#
-#   @@all = []
-#
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def add_song(song)
-#     song.artist = self
-#     @@all << self
-#   end
-#
-#   def songs
-#     Song.all.select {|song| song.artist == self}
-#   end
-#
-#   def self.find_or_create_by_name(name)
-#     self.find(name) || self.new(name)
-#   end
-#
-#   def self.find(name)
-#     self.all.find do |artist| artist.name == name
-#     end
-#   end
-#
-#   def print_songs
-#     songs.select do |song| puts song.name
-#     end
-#   end
-#
-# end
-
+# 
 class Artist
-  @@all = []
   attr_accessor :name, :songs
+
+  @@all = []
 
   def initialize(name)
     @name = name
     save
   end
 
+  def save 
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
 
   def add_song(song)
     song.artist = self
+    @@all << self
   end
 
   def songs
@@ -61,18 +28,55 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.new(name)
+    self.find(name) || self.new(name)
   end
 
   def self.find(name)
-    self.all.find {|artist| artist.name == name }
-  end
-
-  def save
-    @@all << self
+    self.all.find do |artist| artist.name == name
+    end
   end
 
   def print_songs
-    songs.each {|song| puts song.name}
+    songs.select do |song| puts song.name
+    end
   end
+
 end
+
+# class Artist
+#   @@all = []
+#   attr_accessor :name, :songs
+# 
+#   def initialize(name)
+#     @name = name
+#     save
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def add_song(song)
+#     song.artist = self
+#   end
+# 
+#   def songs
+#     Song.all.select {|song| song.artist == self}
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     self.find(name) ? self.find(name) : self.new(name)
+#   end
+# 
+#   def self.find(name)
+#     self.all.find {|artist| artist.name == name }
+#   end
+# 
+#   def save
+#     @@all << self
+#   end
+# 
+#   def print_songs
+#     songs.each {|song| puts song.name}
+#   end
+# end
