23fef03cbd3bcc573281682abd01612b6f6f40b0
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 04:15:21 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,33 +1,17 @@
 
-# class MP3Importer
-#   attr_accessor :path
-#
-#   def initialize(path)
-#     @path = path
-#   end
-#
-#   def files
-#     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/","") }
-#   end
-#
-#   def import
-#     files.each {|name| Song.new_by_filename(name)}
-#   end
-#
-# end
-
 class MP3Importer
-  attr_reader :path
+  attr_accessor :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/","") }
   end
 
   def import
-    files.each{|f| Song.new_by_filename(f)}
+    files.each {|name| Song.new_by_filename(name)}
   end
+
 end
