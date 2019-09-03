a24eab63155655c07d8d834edba3eabc7b1b846f
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 04:02:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,43 @@
-require 'pry'
+# require 'pry'
+# 
+# class Song
+#   attr_accessor :name, :artist
+# 
+#   @@all = []
+# 
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.new_by_filename(filename)
+#     song = filename.split(" - ")[1]
+#     artist = filename.split(" - ")[0]
+#     new_song = Song.new(song)
+#     new_song.artist = artist
+#     new_song
+#   end
+# 
+#   def artist_name= (artist)
+#     @@all << self.artist = Artist.find_or_create_by_name(artist)
+#   end
+# 
+# end
 
 class Song
   attr_accessor :name, :artist
-
   @@all = []
 
   def initialize(name)
     @name = name
+    save
+  end
+
+  def save
     @@all << self
   end
 
@@ -15,15 +46,14 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = filename.split(" - ")[1]
-    artist = filename.split(" - ")[0]
-    new_song = Song.new(song)
-    new_song.artist = artist
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
     new_song
   end
 
-  def artist_name= (artist)
-    @@all << self.artist = Artist.find_or_create_by_name(artist)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
-
 end
